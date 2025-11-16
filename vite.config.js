92536ebd38900f41e5be13e44c542fdceb963881
ssonsy <tjrdhdtjrdhd@gmail.com>
2025-11-16 22:13:36 +0900

    ggg

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/web5/',
   plugins: [react()],
-  base: '/web5/',   // ⬅️ GitHub Pages 하위 경로 설정!!!
 })
